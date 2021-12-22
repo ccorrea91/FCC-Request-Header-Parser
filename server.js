8bad6941098c066d5e4000ce0b218baab584859b
Carlos Correa <ccorrea0491@gmail.com>
2021-12-22 00:09:36 +0000

    Initial commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+// var ip = require("ip");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,6 +25,10 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.get("/api/whoami", function(req, res) {
+  res.json({"ipaddress": req.ip, "language": req.headers['accept-language'], "software": req.headers['user-agent']})
+})
 
 
 
